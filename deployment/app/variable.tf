1d46995591a3ffbe34b619359c7c7fa7cc294d97
femi <femmie009@gmail.com>
2021-11-13 15:49:17 +0300

    added files

---
+++ deployment/app/variable.tf
@@ -0,0 +1,13 @@
+variable "aws_region" {
+  default = "us-east-2"
+  type    = string
+}
+
+variable "aws_subnets_cidr" {
+  default     = ["10.16.1.0/24", "10.16.16.0/24", "10.16.32.0/24"]
+  description = "List of Cidr blocks"
+}
+variable "aws_availability_zones" {
+  default     = ["us-east-2a", "us-east-2b", "us-east-2c"]
+  description = "Availability zone list"
+}
